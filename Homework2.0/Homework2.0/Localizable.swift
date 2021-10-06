74865f5d315d7c270687a76b5fbc622b95ff114b
AkhmadievALbert <a.akhmadiev@tinkoff.ru>
2021-10-06 23:47:39 +0300

    [add] utilities

--- Homework2.0/Homework2.0/Localizable.swift
+++ Homework2.0/Homework2.0/Localizable.swift
@@ -8,6 +8,21 @@
 import Foundation
 
 enum Localizable {
+    
+    // MARK: Profile
+    
+    /// Messages
     static let profilePageMessageButton = "Messages"
+    /// Logout
     static let profileLogoutButton = "Logout"
+    
+    
+    // MARK: Messages
+    
+    /// PINNED
+    static let messagesPinned = "PINNED"
+    /// Who do you want to chat with?
+    static let messagesSearchPlaceholder = "Who do you want to chat with?"
+    /// MESSAGES
+    static let messagesTitle = "MESSAGES"
 }
--- Homework2.0/Homework2.0/Services/DisplayDataService.swift
+++ Homework2.0/Homework2.0/Services/DisplayDataService.swift
@@ -9,6 +9,7 @@ import UIKit
 
 protocol DisplayDataServiceProtocol {
     func displayDataForProfileView(completion: @escaping (Result<ProfileDisplayData, Error>) -> Void)
+    func displayDataForMessagesView(completion: @escaping (Result<MessagesDisplayData, Error>) -> Void)
 }
 
 final class DisplayDataService: DisplayDataServiceProtocol {
@@ -17,13 +18,23 @@ final class DisplayDataService: DisplayDataServiceProtocol {
     
     static let `default`: DisplayDataServiceProtocol = DisplayDataService()
     
+    // MARK: Private
     
+    private let messages: [MessagesDisplayData.Message] = [.init(image: UIImage(named: "ProfilePhoto-1"), firstName: "Jessica", lastName: "Thompson", description: "Hey you! Are u there?", date: "4h ago"),
+                                                           .init(image: UIImage(named: "ProfilePhoto-2"), firstName: "Kat", lastName: "Williams", description: "OMG! OMG! OMG!", date: "5h ago"),
+                                                           .init(image: UIImage(named: "ProfilePhoto-3"), firstName: "Jacob", lastName: "Washington", description: "Sure. Sunday works for me!", date: "20/9/21"),
+                                                           .init(image: nil, firstName: "Leslie", lastName: "Alexander", description: "Sent you an invite for next monday.", date: "19/9/21"),
+                                                           .init(image: UIImage(named: "ProfilePhoto-4"), firstName: "Tony", lastName: "Monta", description: "How’s Alicia doing? Ask her to give m...", date: "19/9/21")]
     
     // MARK: Public
     
     func displayDataForProfileView(completion: @escaping (Result<ProfileDisplayData, Error>) -> Void) {
         completion(.success(ProfileDisplayData(username: "Alex Tsimikas",
                                                city: "Brooklyn, NY")))
+    }
+    
+    func displayDataForMessagesView(completion: @escaping (Result<MessagesDisplayData, Error>) -> Void) {
+        completion(.success(MessagesDisplayData(messages: messages)))
     }
 }
 
